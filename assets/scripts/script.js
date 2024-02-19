4775423c2abd2a4e82655e8d7cfb792373bb0967
kuanJoy <kuanyshmykyev@gmail.com>
2024-02-19 16:15:32 +0600

    fix startgame

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -85,10 +85,12 @@ if (isMobile()) {
   document
     .getElementById("btn-check")
     .addEventListener("touchstart", checkUserAnswer);
+  document.getElementById("btn-back").addEventListener("touchstart", startGame);
 } else {
   document
     .getElementById("btn-check")
     .addEventListener("click", checkUserAnswer);
+  document.getElementById("btn-back").addEventListener("click", startGame);
 }
 
 function checkUserAnswer() {
